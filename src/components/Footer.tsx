c0a6cda5f5f8e0ec88f36c2892c6e2140819df09
pascuajo <joesiphoneusa@gmail.com>
2025-08-21 16:05:55 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,7 +76,7 @@ export function Footer() {
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
           <p>
-            &copy; {new Date().getFullYear()} Clearmont Consulting. All rights reserved.
+            &copy; {new Date().getFullYear()} clearmont consulting llc. all rights reserved.
           </p>
         </div>
       </div>
