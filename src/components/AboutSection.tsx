4dda39dd9c4b38aa455abf02eaf99ce312d2c14e
pascuajo <joesiphoneusa@gmail.com>
2025-08-26 13:43:36 -0500

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -45,7 +45,7 @@ export function AboutSection() {
             <AnimateOnScroll direction="right" className="mb-8">
               <div className="w-64 h-64 rounded-lg overflow-hidden shadow-lg">
                 <img 
-                  src="/profile.png" 
+                  src="/Profile.png" 
                   alt="Joe Pascual - Strategic Innovation Consultant" 
                   className="w-full h-full object-cover" 
                 />
